e01f3060522ac38666b2914e8b3ed1b7798ef4d7
GtrCraft <nickvanbokhorst@hotmail.com>
2021-08-03 11:32:17 +0200

    sm8250-common: AOSIP bringup

--- kona.mk
+++ kona.mk
@@ -240,10 +240,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_kona
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
